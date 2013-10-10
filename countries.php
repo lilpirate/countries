b6f64c09ad202ff1935f432dbc0a9ba16089ff13
Kapeel Sable <kapeel.sable@gmail.com>
2013-10-10 13:29:26 +0530

    added the original file for ref

--- countries.php
+++ countries.php
@@ -1,7 +1,7 @@
 <?php
-$countries = json_decode( file_get_contents( 'countries_source.json' ) );
+$countries = json_decode( file_get_contents( 'countries.json' ) );
 $target = array();
 foreach( $countries as $country ) {
     $target[ $country->cca2 ] = $country->{'calling-code'};
 }
-file_put_contents( 'countries.json', json_encode( $target ) );
+file_put_contents( 'countries_2.json', json_encode( $target ) );
